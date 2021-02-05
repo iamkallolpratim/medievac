682bfc2e797792ebc7570c03f95435514557e2de
kallolpratim <ksaikia615@gmail.com>
2021-02-05 13:21:22 +0530

    first commit

---
+++ _header.php
@@ -0,0 +1,42 @@
+<!--::header part start::-->
+<header class="main_menu home_menu">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.php"> <img class="logo-small" src="img/medievac_logo.png"
+                            alt="logo"> </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse main-menu-item justify-content-center"
+                        id="navbarSupportedContent">
+                        <ul class="navbar-nav align-items-center">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">about</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="Doctor.php">Doctors</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="services.php">services</a>
+                            </li>
+
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <a class="btn_2 d-none d-lg-block" href="#">HOT LINE- 09856</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Header part end-->
---
+++ dep.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html lang="en">
+
+<!-- Mirrored from preview.colorlib.com/theme/medico/dep.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Nov 2020 08:13:28 GMT -->
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>medical</title>
+    <link rel="icon" href="img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="css/flaticon.css">
+    <!-- magnific popup CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <!-- nice select CSS -->
+    <link rel="stylesheet" href="css/nice-select.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!--::header part start::-->
+    <?php include "./_header.php" ?>
+    <!-- Header part end-->
+    
+    <!-- our depertment part start-->
+    <section class="our_depertment section_padding single_pepertment_page">
+        <div class="container">
+            <div class="row justify-content-center text-center">
+                <div class="col-xl-12">
+                    <div class="depertment_content">
+                        <div class="row justify-content-center">
+                            <div class="col-xl-8">
+                                <h2>Our Depertment</h2>
+                                <div class="row">
+                                    <div class="col-lg-6 col-sm-6">
+                                        <div class="single_our_depertment">
+                                            <span class="our_depertment_icon"><img src="img/icon/feature_2.svg"
+                                                    alt=""></span>
+                                            <h4>Better Future</h4>
+                                            <p>Darkness multiply rule Which from without life creature blessed
+                                                give moveth moveth seas make day which divided our have.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6">
+                                        <div class="single_our_depertment">
+                                            <span class="our_depertment_icon"><img src="img/icon/feature_2.svg"
+                                                    alt=""></span>
+                                            <h4>Better Future</h4>
+                                            <p>Darkness multiply rule Which from without life creature blessed
+                                                give moveth moveth seas make day which divided our have.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6">
+                                        <div class="single_our_depertment">
+                                            <span class="our_depertment_icon"><img src="img/icon/feature_2.svg"
+                                                    alt=""></span>
+                                            <h4>Better Future</h4>
+                                            <p>Darkness multiply rule Which from without life creature blessed
+                                                give moveth moveth seas make day which divided our have.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6">
+                                        <div class="single_our_depertment">
+                                            <span class="our_depertment_icon"><img src="img/icon/feature_2.svg"
+                                                    alt=""></span>
+                                            <h4>Better Future</h4>
+                                            <p>Darkness multiply rule Which from without life creature blessed
+                                                give moveth moveth seas make day which divided our have.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- our depertment part end-->
+    <!--::doctor_part start::-->
+    <section class="doctor_part section_padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-8">
+                    <div class="section_tittle text-center">
+                        <h2> Experienced Doctors</h2>
+                        <p>Face replenish sea good winged bearing years air divide wasHave night male also</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_blog_item">
+                        <div class="single_blog_img">
+                            <img src="img/doctor/doctor_1.png" alt="doctor">
+                            <div class="social_icon">
+                                <ul>
+                                    <li><a href="#"> <i class="ti-facebook"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-twitter-alt"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-instagram"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-skype"></i> </a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="single_text">
+                            <div class="single_blog_text">
+                                <h3>DR Adam Billiard</h3>
+                                <p>Heart specialist</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_blog_item">
+                        <div class="single_blog_img">
+                            <img src="img/doctor/doctor_4.png" alt="doctor">
+                            <div class="social_icon">
+                                <ul>
+                                    <li><a href="#"> <i class="ti-facebook"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-twitter-alt"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-instagram"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-skype"></i> </a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="single_text">
+                            <div class="single_blog_text">
+                                <h3>DR Adam Billiard</h3>
+                                <p>Medicine specialist</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_blog_item">
+                        <div class="single_blog_img">
+                            <img src="img/doctor/doctor_2.png" alt="doctor">
+                            <div class="social_icon">
+                                <ul>
+                                    <li><a href="#"> <i class="ti-facebook"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-twitter-alt"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-instagram"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-skype"></i> </a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="single_text">
+                            <div class="single_blog_text">
+                                <h3>DR Fred Macyard</h3>
+                                <p>CHeart specialist</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_blog_item">
+                        <div class="single_blog_img">
+                            <img src="img/doctor/doctor_3.png" alt="doctor">
+                            <div class="social_icon">
+                                <ul>
+                                    <li><a href="#"> <i class="ti-facebook"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-twitter-alt"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-instagram"></i> </a></li>
+                                    <li><a href="#"> <i class="ti-skype"></i> </a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="single_text">
+                            <div class="single_blog_text">
+                                <h3>DR Justin Stuard</h3>
+                                <p>Heart specialist</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--::doctor_part end::-->
+    <!-- footer part start-->
+    <?php include "./_footer.php" ?>
+</body>
+
+<!-- Mirrored from preview.colorlib.com/theme/medico/dep.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Nov 2020 08:13:28 GMT -->
+
+</html>
